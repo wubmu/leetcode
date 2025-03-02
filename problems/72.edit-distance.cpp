974355f27b93831e5c0ae5a12c2f15a16b79e36b
Yabin Wu <923185028@qq.com>
2025-03-02 16:42:49 +0000

    [add]72编辑距离

---
+++ problems/72.edit-distance.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=72 lang=cpp
+ *
+ * [72] Edit Distance
+ */
+
+// @lc code=start
+#include<vector>
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        vector<vertor<int>> dp (n + 1, m +1)
+        
+    }
+};
+// @lc code=end
+
